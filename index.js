e7322120fceb370ae4f16dae391996336ddbcab6
Patrick Wu <Ptrckwu@yahoo.com>
2021-08-31 23:33:43 -0400

    biig brain time

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+const returnFirstTwoDrivers = () => drivers.slice(0,2)
+const returnLastTwoDrivers = () => drivers.slice(2,4)
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(integer) {
+    return ((fare) => integer * fare);
+    }
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+function selectDifferentDrivers(driverArray, returnDriversParameter) {
+    return returnDriversParameter(driverArray)
+}
